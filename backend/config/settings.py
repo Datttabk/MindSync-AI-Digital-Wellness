8bcfccede2aca2736862a65c3081284d290afc63
Datta Kambagi <dattakambagi021@gmail.com>
2026-06-28 01:56:40 +0530

    Change ALLOWED_CORS_ORIGINS to use type hinting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,11 +4,13 @@ class Settings(BaseSettings):
     APP_NAME: str = "MindSync AI Decision Support API"
     DEBUG: bool = True
     API_PREFIX: str = "/api"
-    ALLOWED_CORS_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://mindsync-ai-digital-wellnessfrontend.onrender.com",
-]
+
+    ALLOWED_CORS_ORIGINS: list[str] = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://mindsync-ai-digital-wellnessfrontend.onrender.com",
+    ]
+
     class Config:
         env_file = ".env"
 
